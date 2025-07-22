799e11583e2e7597784cc3d4e2ad923acdd45c00
Enzoait <pcenzox25@gmail.com>
2025-07-22 15:51:10 +0200

    ECR

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,11 @@
+resource "aws_ecr_repository" "frontend" {
+  name = "projetfilrouge-frontend"
+}
+
+resource "aws_ecr_repository" "backend" {
+  name = "projetfilrouge-backend"
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
   tags   = {
