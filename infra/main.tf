fb7057ad4e483408698134b0964a85370ab5221f
Enzoait <pcenzox25@gmail.com>
2025-07-22 16:22:27 +0200

    ECR 2

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,19 @@
 resource "aws_ecr_repository" "frontend" {
   name = "projetfilrouge-frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
 resource "aws_ecr_repository" "backend" {
   name = "projetfilrouge-backend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
 resource "aws_s3_bucket" "example" {
