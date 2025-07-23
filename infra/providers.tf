86a76c3abd3392227e4ec71c02d110c120b98b70
Enzoait <pcenzox25@gmail.com>
2025-07-23 16:44:43 +0200

    backend terraform

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-backend-terraformbackends3bucket-kmf8fwf9q4zr"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-backend-TerraformBackendDynamoDBTable-1H2S07OFDUIEQ"
+  }
 }
 
 # Configure the AWS Provider
