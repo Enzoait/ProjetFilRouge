71e1e6e660f24ef40540f8560b04cd36d4c11603
Enzoait <pcenzox25@gmail.com>
2025-07-25 16:15:54 +0200

    lambda api_gateway

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -57,10 +57,16 @@ resource "aws_api_gateway_deployment" "dynamodb_manager" {
     #       resources will show a difference after the initial implementation.
     #       It will stabilize to only change when resources change afterwards.
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.dynamodb_manager.id,
-      aws_api_gateway_method.post_method.id,
-      aws_api_gateway_integration.post_method.id,
-    ]))
+    aws_api_gateway_resource.dynamodb_manager.id,
+    aws_api_gateway_method.post_method.id,
+    aws_api_gateway_integration.post_method.id,
+    aws_api_gateway_method.get_method.id,
+    aws_api_gateway_integration.get_method.id,
+    aws_api_gateway_method.put_method.id,
+    aws_api_gateway_integration.put_method.id,
+    aws_api_gateway_method.delete_method.id,
+    aws_api_gateway_integration.delete_method.id,
+  ]))
   }
 
   lifecycle {
@@ -110,54 +116,153 @@ resource "aws_api_gateway_integration_response" "post_method" {
   ]
 }
 
+resource "aws_api_gateway_method" "get_method" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
+}
+
+resource "aws_api_gateway_method_response" "get_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration" "get_method" {
+  http_method             = aws_api_gateway_method.get_method.http_method
+  resource_id             = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "get_method" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.get_method.http_method
+  status_code = aws_api_gateway_method_response.get_response_200.status_code
+
+  depends_on = [aws_api_gateway_integration.get_method]
+}
+
+
+resource "aws_api_gateway_method" "put_method" {
+  authorization = "NONE"
+  http_method   = "PUT"
+  resource_id   = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
+}
+
+resource "aws_api_gateway_method_response" "put_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.put_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration" "put_method" {
+  http_method             = aws_api_gateway_method.put_method.http_method
+  resource_id             = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "put_method" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.put_method.http_method
+  status_code = aws_api_gateway_method_response.put_response_200.status_code
+
+  depends_on = [aws_api_gateway_integration.put_method]
+}
+
+
+resource "aws_api_gateway_method" "delete_method" {
+  authorization = "NONE"
+  http_method   = "DELETE"
+  resource_id   = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
+}
+
+resource "aws_api_gateway_method_response" "delete_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.delete_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration" "delete_method" {
+  http_method             = aws_api_gateway_method.delete_method.http_method
+  resource_id             = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "delete_method" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.delete_method.http_method
+  status_code = aws_api_gateway_method_response.delete_response_200.status_code
+
+  depends_on = [aws_api_gateway_integration.delete_method]
+}
+
+
 # OPTIONS FOR CORS
 
-# resource "aws_api_gateway_method" "option_method" {
-#   authorization = "NONE"
-#   http_method   = "OPTIONS"
-#   resource_id   = aws_api_gateway_resource.dynamodb_manager.id
-#   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
-# }
+resource "aws_api_gateway_method" "option_method" {
+  authorization = "NONE"
+  http_method   = "OPTIONS"
+  resource_id   = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
+}
 
 
-# resource "aws_api_gateway_integration" "options_method" {
-#   http_method = aws_api_gateway_method.post_method.http_method
-#   resource_id = aws_api_gateway_resource.dynamodb_manager.id
-#   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
-#    integration_http_method = "POST"
-#    type                    = "MOCK"
-# }
-# resource "aws_api_gateway_method_response" "options" {
-#   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
-#   resource_id = aws_api_gateway_resource.dynamodb_manager.id
-#   http_method = aws_api_gateway_method.option_method.http_method
-#   status_code = "200"
+resource "aws_api_gateway_integration" "options_method" {
+  http_method = aws_api_gateway_method.post_method.http_method
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+   integration_http_method = "POST"
+   type                    = "MOCK"
+}
+resource "aws_api_gateway_method_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.option_method.http_method
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" = false
-#     "method.response.header.Access-Control-Allow-Methods" = false
-#     "method.response.header.Access-Control-Allow-Origin"  = false
-#   }
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = false
+    "method.response.header.Access-Control-Allow-Methods" = false
+    "method.response.header.Access-Control-Allow-Origin"  = false
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
 
-# }
+}
 
-# resource "aws_api_gateway_integration_response" "options" {
-#   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
-#   resource_id = aws_api_gateway_resource.dynamodb_manager.id
-#   http_method = aws_api_gateway_method.option_method.http_method
-#   status_code = aws_api_gateway_method_response.options.status_code
+resource "aws_api_gateway_integration_response" "options" {
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.dynamodb_manager.id
+  http_method = aws_api_gateway_method.option_method.http_method
+  status_code = aws_api_gateway_method_response.options.status_code
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-#     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-#     "method.response.header.Access-Control-Allow-Origin" = "'*'"
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+}
 
-#   depends_on = [
-#     aws_api_gateway_method.option_method,
-#     aws_api_gateway_integration.options_method
-#   ]
-# }
+  depends_on = [
+    aws_api_gateway_method.option_method,
+    aws_api_gateway_integration.options_method
+  ]
+}
