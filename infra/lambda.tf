9dee2f35c06de840e1633440c239285fade5be15
Enzoait <pcenzox25@gmail.com>
2025-07-25 23:18:22 +0200

    fix

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -88,7 +88,7 @@ resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
 }
 
-resource "aws_api_gateway_method_response" "post_response_200" {
+resource "aws_api_gateway_method_response" "post_method_response_200" {
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id = aws_api_gateway_resource.dynamodb_manager.id
   http_method = aws_api_gateway_method.post_method.http_method
