ede3051ddfa39f350920932d51ee93314230d031
Enzoait <pcenzox25@gmail.com>
2025-07-26 16:23:43 +0200

    updated routes

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -20,12 +20,10 @@ resource "aws_lambda_function" "lambda_function_over_https" {
   environment {
     variables = {
       TABLE_NAME = "todos"
-      # Change par la vraie table si besoin
     }
   }
 }
 
-# API Gateway REST API
 resource "aws_api_gateway_rest_api" "dynamo_db_operations" {
   name        = "DynamoDBOperations"
   description = "API pour les opérations sur DynamoDB"
@@ -35,7 +33,6 @@ resource "aws_api_gateway_rest_api" "dynamo_db_operations" {
   tags = var.tags
 }
 
-# Resources pour /todos et /todos/{id}
 resource "aws_api_gateway_resource" "todos" {
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   parent_id   = aws_api_gateway_rest_api.dynamo_db_operations.root_resource_id
@@ -48,141 +45,189 @@ resource "aws_api_gateway_resource" "todos_id" {
   path_part   = "{id}"
 }
 
-# -- /todos - GET
+# --- GET /todos ---
+
 resource "aws_api_gateway_method" "get_todos" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos.id
   http_method   = "GET"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "get_todos" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos.id
-  http_method         = "GET"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+  depends_on  = [aws_api_gateway_method.get_todos]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos.id
+  http_method = aws_api_gateway_method.get_todos.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
+
 resource "aws_api_gateway_integration" "get_todos" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos.id
-  http_method             = "GET"
+  http_method             = aws_api_gateway_method.get_todos.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
 }
+
 resource "aws_api_gateway_integration_response" "get_todos" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos.id
-  http_method         = "GET"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "'*'" }
-  depends_on          = [aws_api_gateway_integration.get_todos]
+  depends_on = [aws_api_gateway_method.get_todos, aws_api_gateway_integration.get_todos]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos.id
+  http_method = aws_api_gateway_method.get_todos.http_method
+  status_code = aws_api_gateway_method_response.get_todos.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
-# -- /todos - POST
+# --- POST /todos ---
+
 resource "aws_api_gateway_method" "post_todos" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos.id
   http_method   = "POST"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "post_todos" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos.id
-  http_method         = "POST"
-  status_code         = "201"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+  depends_on  = [aws_api_gateway_method.post_todos]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos.id
+  http_method = aws_api_gateway_method.post_todos.http_method
+  status_code = "201"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
+
 resource "aws_api_gateway_integration" "post_todos" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos.id
-  http_method             = "POST"
+  http_method             = aws_api_gateway_method.post_todos.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
 }
+
 resource "aws_api_gateway_integration_response" "post_todos" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos.id
-  http_method         = "POST"
-  status_code         = "201"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "'*'" }
-  depends_on          = [aws_api_gateway_integration.post_todos]
+  depends_on = [aws_api_gateway_method.post_todos, aws_api_gateway_integration.post_todos]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos.id
+  http_method = aws_api_gateway_method.post_todos.http_method
+  status_code = aws_api_gateway_method_response.post_todos.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
-# -- /todos/{id} - PUT
+# --- PUT /todos/{id} ---
+
 resource "aws_api_gateway_method" "put_todos_id" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos_id.id
   http_method   = "PUT"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "put_todos_id" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos_id.id
-  http_method         = "PUT"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+  depends_on  = [aws_api_gateway_method.put_todos_id]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos_id.id
+  http_method = aws_api_gateway_method.put_todos_id.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
+
 resource "aws_api_gateway_integration" "put_todos_id" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos_id.id
-  http_method             = "PUT"
+  http_method             = aws_api_gateway_method.put_todos_id.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
 }
+
 resource "aws_api_gateway_integration_response" "put_todos_id" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos_id.id
-  http_method         = "PUT"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "'*'" }
-  depends_on          = [aws_api_gateway_integration.put_todos_id]
+  depends_on = [aws_api_gateway_method.put_todos_id, aws_api_gateway_integration.put_todos_id]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos_id.id
+  http_method = aws_api_gateway_method.put_todos_id.http_method
+  status_code = aws_api_gateway_method_response.put_todos_id.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
-# -- /todos/{id} - DELETE
+# --- DELETE /todos/{id} ---
+
 resource "aws_api_gateway_method" "delete_todos_id" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos_id.id
   http_method   = "DELETE"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "delete_todos_id" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos_id.id
-  http_method         = "DELETE"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
+  depends_on  = [aws_api_gateway_method.delete_todos_id]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos_id.id
+  http_method = aws_api_gateway_method.delete_todos_id.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
+
 resource "aws_api_gateway_integration" "delete_todos_id" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos_id.id
-  http_method             = "DELETE"
+  http_method             = aws_api_gateway_method.delete_todos_id.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda_function_over_https.invoke_arn
 }
+
 resource "aws_api_gateway_integration_response" "delete_todos_id" {
-  rest_api_id         = aws_api_gateway_rest_api.dynamo_db_operations.id
-  resource_id         = aws_api_gateway_resource.todos_id.id
-  http_method         = "DELETE"
-  status_code         = "200"
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "'*'" }
-  depends_on          = [aws_api_gateway_integration.delete_todos_id]
+  depends_on = [aws_api_gateway_method.delete_todos_id, aws_api_gateway_integration.delete_todos_id]
+
+  rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
+  resource_id = aws_api_gateway_resource.todos_id.id
+  http_method = aws_api_gateway_method.delete_todos_id.http_method
+  status_code = aws_api_gateway_method_response.delete_todos_id.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
-# -- /todos (OPTIONS pour CORS)
+# --- OPTIONS /todos ---
+
 resource "aws_api_gateway_method" "options_todos" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "options_todos" {
+  depends_on = [aws_api_gateway_method.options_todos]
+
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id = aws_api_gateway_resource.todos.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.options_todos.http_method
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = true
@@ -190,40 +235,47 @@ resource "aws_api_gateway_method_response" "options_todos" {
     "method.response.header.Access-Control-Allow-Methods" = true
   }
 }
+
 resource "aws_api_gateway_integration" "options_todos" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos.id
-  http_method             = "OPTIONS"
+  http_method             = aws_api_gateway_method.options_todos.http_method
   integration_http_method = "OPTIONS"
   type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
+
 resource "aws_api_gateway_integration_response" "options_todos" {
+  depends_on = [aws_api_gateway_method.options_todos, aws_api_gateway_integration.options_todos]
+
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id = aws_api_gateway_resource.todos.id
-  http_method = "OPTIONS"
-  status_code = "200"
+  http_method = aws_api_gateway_method.options_todos.http_method
+  status_code = aws_api_gateway_method_response.options_todos.status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
   }
-  depends_on = [aws_api_gateway_integration.options_todos]
 }
 
-# -- /todos/{id} (OPTIONS pour CORS)
+# --- OPTIONS /todos/{id} ---
+
 resource "aws_api_gateway_method" "options_todos_id" {
   rest_api_id   = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id   = aws_api_gateway_resource.todos_id.id
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
+
 resource "aws_api_gateway_method_response" "options_todos_id" {
+  depends_on = [aws_api_gateway_method.options_todos_id]
+
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id = aws_api_gateway_resource.todos_id.id
-  http_method = "OPTIONS"
+  http_method = aws_api_gateway_method.options_todos_id.http_method
   status_code = "200"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = true
@@ -231,30 +283,34 @@ resource "aws_api_gateway_method_response" "options_todos_id" {
     "method.response.header.Access-Control-Allow-Methods" = true
   }
 }
+
 resource "aws_api_gateway_integration" "options_todos_id" {
   rest_api_id             = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id             = aws_api_gateway_resource.todos_id.id
-  http_method             = "OPTIONS"
+  http_method             = aws_api_gateway_method.options_todos_id.http_method
   integration_http_method = "OPTIONS"
   type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
+
 resource "aws_api_gateway_integration_response" "options_todos_id" {
+  depends_on = [aws_api_gateway_method.options_todos_id, aws_api_gateway_integration.options_todos_id]
+
   rest_api_id = aws_api_gateway_rest_api.dynamo_db_operations.id
   resource_id = aws_api_gateway_resource.todos_id.id
-  http_method = "OPTIONS"
-  status_code = "200"
+  http_method = aws_api_gateway_method.options_todos_id.http_method
+  status_code = aws_api_gateway_method_response.options_todos_id.status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'PUT,DELETE,OPTIONS'"
   }
-  depends_on = [aws_api_gateway_integration.options_todos_id]
 }
 
-# CloudWatch Log Group, policy, attachements...
+# === CloudWatch Logs ===
+
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${aws_lambda_function.lambda_function_over_https.function_name}"
   retention_in_days = 14
